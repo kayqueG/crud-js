842c9ed71ed4415bb53d19dc887534ef81cc3b88
kayque <kayquegps2@gmail.com>
2021-11-28 19:42:59 -0300

    Métodos create,read,update e delete + interação do cliente no cadastro salvando no localstorage finalizados!

--- main.js
+++ main.js
@@ -3,12 +3,16 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+const closeModal = () => {
+    limparCampos()
+    document.getElementById('modal')
+        .classList.remove('active')
+
+}
 
 
 const tempCliente = {
-    nome: "Kayque",
+    nome: "kayque",
     email: "kayque@gmail.com",
     celular: "(11) 99090-9090",
     cidade: "Santo André"
@@ -20,6 +24,25 @@ const getLocalStorage = () => JSON.parse(localStorage.getItem("db_cliente")) ?? 
 const setLocalStorage = (db_cliente) => localStorage.setItem("db_cliente", JSON.stringify(db_cliente))
 
 
+//DELETE
+const deleteCliente = (index) => {
+    const db_cliente = readCliente()
+    db_cliente.splice(index, 1)
+    setLocalStorage(db_cliente)
+}
+
+//UPDATE
+const updateCliente = (index, cliente) => {
+    const db_cliente = readCliente()
+    db_cliente[index] = cliente
+    setLocalStorage(db_cliente)
+}
+
+
+//READ
+const readCliente = () => getLocalStorage()
+
+
 //CREATE
 const createClient = (cliente) => {
 
@@ -27,6 +50,30 @@ const createClient = (cliente) => {
     db_cliente.push(cliente)
     setLocalStorage(db_cliente)
 
+}
+
+const camposValidos = () => {
+    return document.getElementById("form").reportValidity() // retorna verdadeiro se todos os requisitos do html foram atendidos
+}
+
+const limparCampos = () => {
+    const campos = document.querySelectorAll('.modal-field')
+    campos.forEach(campo => campo.value = "") // deixando cada campo vazio depois do cadastro
+}
+
+//Interação com o usuario
+const saveCliente = () => {
+    if (camposValidos()) {
+        const cliente = {
+            nome: document.getElementById("nome").value,
+            email: document.getElementById("email").value,
+            celular: document.getElementById("celular").value,
+            cidade: document.getElementById("cidade").value
+
+        }
+        createClient(cliente)
+        closeModal()
+    }
 }
 
 
@@ -37,3 +84,5 @@ document.getElementById('cadastrarCliente') // abrir o modal quando o usuario cl
 
 document.getElementById('modalClose')  // fechar o modal quando o usuario clicar em  fechar
     .addEventListener('click', closeModal)
+
+document.getElementById('salvar').addEventListener('click', saveCliente)
