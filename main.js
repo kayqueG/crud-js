1b3f56c9106ecb50d70315d9c16b7e048ab71de8
kayque <kayquegps2@gmail.com>
2021-11-29 20:59:02 -0300

    metodos para editar,excluir e aparecer os clientes cadastrados na pagina finalizados

--- main.js
+++ main.js
@@ -10,41 +10,32 @@ const closeModal = () => {
 
 }
 
-
-const tempCliente = {
-    nome: "kayque",
-    email: "kayque@gmail.com",
-    celular: "(11) 99090-9090",
-    cidade: "Santo André"
-}
-
-
 const getLocalStorage = () => JSON.parse(localStorage.getItem("db_cliente")) ?? [] // passando de string para objeto de novo e verificando se é nulo, se for, retorna um array vazio
 
 const setLocalStorage = (db_cliente) => localStorage.setItem("db_cliente", JSON.stringify(db_cliente))
 
 
 //DELETE
-const deleteCliente = (index) => {
-    const db_cliente = readCliente()
+const deletarCliente = (index) => {
+    const db_cliente = lerCliente()
     db_cliente.splice(index, 1)
     setLocalStorage(db_cliente)
 }
 
 //UPDATE
-const updateCliente = (index, cliente) => {
-    const db_cliente = readCliente()
+const atualizarCliente = (index, cliente) => {
+    const db_cliente = lerCliente()
     db_cliente[index] = cliente
     setLocalStorage(db_cliente)
 }
 
 
 //READ
-const readCliente = () => getLocalStorage()
+const lerCliente = () => getLocalStorage()
 
 
 //CREATE
-const createClient = (cliente) => {
+const criarCliente = (cliente) => {
 
     const db_cliente = getLocalStorage()
     db_cliente.push(cliente)
@@ -59,10 +50,11 @@ const camposValidos = () => {
 const limparCampos = () => {
     const campos = document.querySelectorAll('.modal-field')
     campos.forEach(campo => campo.value = "") // deixando cada campo vazio depois do cadastro
+    document.getElementById('nome').dataset.index = 'new'
 }
 
 //Interação com o usuario
-const saveCliente = () => {
+const salvarCliente = () => {
     if (camposValidos()) {
         const cliente = {
             nome: document.getElementById("nome").value,
@@ -71,13 +63,81 @@ const saveCliente = () => {
             cidade: document.getElementById("cidade").value
 
         }
-        createClient(cliente)
-        closeModal()
+        const index = document.getElementById("nome").dataset.index
+        if (index == "new") {
+            criarCliente(cliente)
+            atualizarTabela()
+            closeModal()
+        } else {
+            atualizarCliente(index, cliente)
+            atualizarTabela()
+            closeModal()
+        }
     }
 }
 
+const criarLinha = (cliente, index) => {
+    const novaLinha = document.createElement('tr')
+    novaLinha.innerHTML = `
+    <td>${cliente.nome}</td>
+    <td>${cliente.email}</td>
+    <td>${cliente.celular}</td>
+     <td>${cliente.cidade}</td>
+     <td>
+        <button id="editar-${index}" type="button" class="button green" > editar</button>
+        <button id="deletar-${index}" type="button" class="button red"> excluir</button>
+        </td>
+     `
+    document.querySelector('#tableClient>tbody').appendChild(novaLinha) // inserindo no tbody
 
+}
 
+const limparTabela = () => {
+    const linhas = document.querySelectorAll('#tableClient>tbody tr')
+    linhas.forEach(linha => linha.parentNode.removeChild(linha))
+}
+const atualizarTabela = () => {
+    const db_cliente = lerCliente()
+    limparTabela()
+    db_cliente.forEach(criarLinha)
+}
+
+const preencherCampos = (cliente) => {
+    document.getElementById("nome").value = cliente.nome
+    document.getElementById("email").value = cliente.email
+    document.getElementById("celular").value = cliente.celular
+    document.getElementById("cidade").value = cliente.cidade
+    document.getElementById("nome").dataset.index = cliente.index
+}
+
+const editarCliente = (index) => {
+
+    const cliente = lerCliente()[index]
+    cliente.index = index
+    preencherCampos(cliente)
+    openModal()
+}
+
+const editarDeletar = (evento) => {
+    if (evento.target.type == 'button') {
+        const [action, index] = evento.target.id.split('-')
+
+        if (action == 'editar') {
+            editarCliente(index)
+        } else {
+            const cliente = lerCliente()[index]
+            const response = confirm(`Você tem certeza que deseja excluir o cliente: ${cliente.nome}`)//Confirmando se o usuario quer deletar mesmo
+            if (response) {
+                deletarCliente(index)
+                atualizarTabela()
+            }
+
+        }
+    }
+
+}
+
+atualizarTabela()
 
 document.getElementById('cadastrarCliente') // abrir o modal quando o usuario clicar em cadastrar cliente
     .addEventListener('click', openModal)
@@ -85,4 +145,6 @@ document.getElementById('cadastrarCliente') // abrir o modal quando o usuario cl
 document.getElementById('modalClose')  // fechar o modal quando o usuario clicar em  fechar
     .addEventListener('click', closeModal)
 
-document.getElementById('salvar').addEventListener('click', saveCliente)
+document.getElementById('salvar').addEventListener('click', salvarCliente)
+
+document.querySelector('#tableClient>tbody').addEventListener('click', editarDeletar)
